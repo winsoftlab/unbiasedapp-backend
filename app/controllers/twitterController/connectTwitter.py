05c162063bcf9d99bf11a624114a32ccafb57f57
Akachi <anabantiakachi1@gmail.com>
2022-08-01 05:51:53 -0700

    fixed twiiter environ key

--- app/controllers/twitterController/connectTwitter.py
+++ app/controllers/twitterController/connectTwitter.py
@@ -3,10 +3,8 @@ import tweepy as tw
 from config import Config
 
 def connect_api():
-    
-    auth = tw.OAuthHandler(os.getenv('TWITTER_KEY'), os.getenv('TWITTER_SECRET'))
+    auth = tw.OAuthHandler(os.environ.get('TWITTER_KEY'), os.environ.get('TWITTER_SECRET'))
 
     twitterApi = tw.API(auth, wait_on_rate_limit=True)
 
     return twitterApi
-
