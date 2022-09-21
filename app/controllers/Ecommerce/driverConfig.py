be691301e4798aa38f7e2e98437f94e28d984476
Winsoft-Lab <christieslab@gmail.com>
2022-09-21 14:21:32 +0100

    heroku gecko config fixes

--- app/controllers/Ecommerce/driverConfig.py
+++ app/controllers/Ecommerce/driverConfig.py
@@ -13,6 +13,7 @@ def set_driver_config():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument('--incoginito')
     options.add_argument('-lang=en-US')
-    driver = webdriver.Firefox(options=options, executable_path=Config.GECKODRIVER_PATH,firefox_binary=FirefoxBinary(Config.FIREFOX_BIN))
+    driver = webdriver.Firefox(options=options)
     return driver
 
+#, executable_path=Config.GECKODRIVER_PATH,firefox_binary=FirefoxBinary(Config.FIREFOX_BIN)
